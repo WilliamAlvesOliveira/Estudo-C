0d82e55f33a29c9cd299b95004366650ebccf39d
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-02-11 13:26:40 -0300

    feat
    adicionar exercício cadastro de aluno

---
+++ Hora-de-codar/cadastroAluno.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int idade, matricula; //podemos declarar variavéis do mesmo tipo em uma única linha.
+    float altura;
+    char nome[50];
+    char exit;
+
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+
+    printf("digite a sua altura: ");
+    scanf("%f", &altura);
+
+    printf("Digite o seu primeiro nome: ");
+    scanf("%s", nome);
+
+    printf("Digite a sua matricula: ");
+    scanf("%d", &matricula);
+
+    printf("----- ALUNO: %s -----\n", nome);
+    printf("IDADE: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("MATRICULA: %d\n", matricula);
+    printf("============= presione 's' para sair: ");
+    scanf(" %c", &exit); 
+
+
+    return 0;
+};
