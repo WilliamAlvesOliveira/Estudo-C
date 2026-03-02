4a5c5d9423ea9475cd96986ee5f00bd6eae1737f
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-03-02 12:07:48 -0300

    refactor
    adicionar arquivo config.h

---
+++ Jokenpo/include/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define TXT_BLUE "\x1b[34m"
+#define TXT_GREEN "\x1b[32m"
+#define TXT_YELLOW "\x1b[33m"
+#define TXT_RED "\x1b[31m"
+
+#define ANSI_RESET "\x1b[0m"
+
+#define BG_RED "\x1b[41m"
+#define BG_GREEN "\x1b[42m"
+#define BG_YELLOW "\x1b[43m"
+
+#endif
--- Jokenpo/src/jokenpo_v_0.1.c
+++ Jokenpo/src/jokenpo_v_0.1.c
@@ -2,55 +2,47 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-
-#define txt_blue "\x1b[34m"
-#define txt_green "\x1b[32m"
-#define txt_yellow "\x1b[33m"
-#define txt_red "\x1b[31m"
-#define ansi_reset "\x1b[0m"
-#define bg_red "\x1b[41m"
-#define bg_green "\x1b[42m"
-#define bg_yellow "\x1b[43m"
+#include "../include/config.h"
 
 int main(){
     int player, comp;
 
     srand(time(0));
 
-    printf(txt_blue);
+    printf(TXT_BLUE);
     printf("|=============================================|\n");
     printf("|                --- JOKENPÔ ---              |\n");
     printf("|=============================================|\n");
 
     printf("|                                             |\n");
-    printf("|%s[1]%s: PEDRA                                   %s|\n",txt_red, ansi_reset, txt_blue);
-    printf("|%s[2]%s: PAPEL                                   %s|\n",txt_red, ansi_reset, txt_blue);
-    printf("|%s[3]%s: TESOURA                                 %s|\n",txt_red, ansi_reset, txt_blue);
+    printf("|%s[1]%s: PEDRA                                   %s|\n",TXT_RED, ANSI_RESET, TXT_BLUE);
+    printf("|%s[2]%s: PAPEL                                   %s|\n",TXT_RED, ANSI_RESET, TXT_BLUE);
+    printf("|%s[3]%s: TESOURA                                 %s|\n",TXT_RED, ANSI_RESET, TXT_BLUE);
     printf("|---------------------------------------------|\n");
-    printf("|                         %sEscolha sua opção: ",ansi_reset);
-    printf(txt_green);
+    printf("|                         %sEscolha sua opção: ",ANSI_RESET);
+    printf(TXT_GREEN);
     scanf("%d", &player);
-    printf(txt_blue);
+    printf(TXT_BLUE);
 
     if(player != 1 && player != 2 && player != 3){
-        printf("| %sOpção Inválida                              %s|\n", txt_red, txt_blue);
+        printf("| %sOpção Inválida                              %s|\n", TXT_RED, TXT_BLUE);
         Sleep(1000);
         printf("|---------------------------------------------|\n");
         printf("|                                             |\n");
         printf("|                   FIM DE JOGO.              |\n");
         printf("|=============================================|\n");
-        printf(ansi_reset);
+        printf(ANSI_RESET);
         return 0;
     }
 
     printf("|---------------------------------------------|\n");
     printf("|                                             |\n");
     Sleep(1000);
-    printf("|                   %s JO", bg_red);
+    printf("|                   %s JO", BG_RED);
     Sleep(1000);
-    printf("%sKEN", bg_yellow);
+    printf("%sKEN", BG_YELLOW);
     Sleep(1000);
-    printf("%sPÔ %s                 %s|\n", bg_green, ansi_reset, txt_blue);
+    printf("%sPÔ %s                 %s|\n", BG_GREEN, ANSI_RESET, TXT_BLUE);
     Sleep(500);
     printf("|                                             |\n");
 
@@ -58,15 +50,15 @@ int main(){
 
     switch(player){
         case 1:
-            printf("|%s Player :   PEDRA",ansi_reset);
+            printf("|%s Player :   PEDRA",ANSI_RESET);
         break;
         
         case 2:
-            printf("|%s Player :   PAPEL",ansi_reset);
+            printf("|%s Player :   PAPEL",ANSI_RESET);
         break;
         
         case 3:
-            printf("|%s Player : TESOURA",ansi_reset);
+            printf("|%s Player : TESOURA",ANSI_RESET);
         break;            
     }
 
@@ -74,15 +66,15 @@ int main(){
 
     switch(comp){
         case 1:
-            printf("PEDRA   : Computador %s|\n",txt_blue);
+            printf("PEDRA   : Computador %s|\n",TXT_BLUE);
         break;
         
         case 2:
-            printf("PAPEL   : Computador %s|\n",txt_blue);
+            printf("PAPEL   : Computador %s|\n",TXT_BLUE);
         break;
         
         case 3:
-            printf("TESOURA : Computador %s|\n",txt_blue);
+            printf("TESOURA : Computador %s|\n",TXT_BLUE);
         break;
     }
 
@@ -91,17 +83,17 @@ int main(){
     Sleep(1000);
     printf("|                                             |\n");
     if(player == comp){
-        printf("|%s EMPATE!!!                                   %s|\n", txt_yellow, txt_blue);
+        printf("|%s EMPATE!!!                                   %s|\n", TXT_YELLOW, TXT_BLUE);
     }else{
-        printf("| %sVENCEDOR: ",ansi_reset);
+        printf("| %sVENCEDOR: ",ANSI_RESET);
         if( 
             player == 1 && comp == 3 || 
             player == 2 && comp == 1 || 
             player == 3 && comp == 2
         ){
-            printf("%sPlayer!!!                         %s|\n",txt_green, txt_blue);
+            printf("%sPlayer!!!                         %s|\n",TXT_GREEN, TXT_BLUE);
         }else{
-            printf("%sComputador!!!                     %s|\n",txt_red, txt_blue);
+            printf("%sComputador!!!                     %s|\n",TXT_RED, TXT_BLUE);
         }
     }
     
@@ -111,6 +103,6 @@ int main(){
     printf("|                                             |\n");
     printf("|                 FIM DE JOGO.                |\n");
     printf("|=============================================|\n");
-    printf(ansi_reset);
+    printf(ANSI_RESET);
     return 0;
 }
